2d67e2de91331091ef06984f5349f07ed150caf0
Sudhamsu <54114483+sudhamsugurijala@users.noreply.github.com>
2020-01-18 15:34:56 +0530

    Add files via upload

---
+++ lab3/validate.js
@@ -0,0 +1,32 @@
+function validateName() {
+
+ var nameDef = new RegExp('^[a-z]$');
+ name = document.getElementById("nameInput").value;
+ if (nameDef.test(name)==true) {
+  alert(" Valid Input ");
+ }
+ else {
+  alert(" Invalid Input ");
+ }
+
+}
+
+function validatePh() {
+
+}
+
+function validateAge() {
+
+}
+
+function validateMail() {
+
+}
+
+function validateDate() {
+
+}
+
+function submitDet() {
+
+}
